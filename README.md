40c4a80b988b15984bd5165ee67b715da69f1b04
apedley <apedley@gmail.com>
2016-03-02 22:21:56 -0600

    Finished login, get, started post

---
+++ README.md
@@ -0,0 +1,4 @@
+## Salesforce REST
+
+Salesforce REST is a module for interfacing with the Salesforce REST API.
+
--- test/salesforce-rest.js
+++ test/salesforce-rest.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var salesforceRest = require('../lib/salesforce-rest');
 var config = require('./config');
+var Promise = require('bluebird');
 
 describe('salesforceRest', function() {
 
@@ -36,7 +37,7 @@ describe('salesforceRest', function() {
 
     it('should login', function() {
       salesforceRest.setOptions(config);
-      salesforceRest.login(function(data) {
+      salesforceRest.login(function(error, data) {
         expect(data).to.have.property('access_token');
         var options = salesforceRest.getOptions();
         expect(options).to.have.property('accessToken');
@@ -45,7 +46,7 @@ describe('salesforceRest', function() {
   });
 
   describe('get', function() {
-    before(function() {
+    beforeEach(function() {
       salesforceRest.resetOptions();
     });
 
@@ -56,8 +57,35 @@ describe('salesforceRest', function() {
 
       expect(getFunction).to.throw(Error);
     });
+
+    it('should run query after login', function() {
+
+      salesforceRest.setOptions(config);
+
+      var loginPromise = Promise.promisify(salesforceRest.login, {context: salesforceRest});
+
+      loginPromise().then(function(rest){
+      })
+      .then(function() {
+        salesforceRest.get('Select Name FROM User', function(error, data) {
+          expect(error).to.be.null;
+          data = JSON.parse(data);
+          expect(Object.keys(data).length).to.be.gt(0);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+    });
   });
 
+  describe('post', function() {
+    it('should not post if options are not set', function() {
+      
+    });
 
+    it('should post after login', function() {
 
+    });
+  });
 });
